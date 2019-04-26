966786145f25bef318fc06a29358da8cee7dabbf
Mario Reder <mreder1289@gmail.com>
2019-04-26 07:07:59 +0200

    remove unused dependencies

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,10 @@
-#[macro_use]
-extern crate relm;
-#[macro_use]
-extern crate relm_derive;
-
 mod core;
-mod frontend;
+// mod frontend;
 mod mupen;
 mod platform;
 
 use crate::core::app::App as Core;
-use crate::frontend::app::App as Frontend;
-
-use relm::Widget;
+// use crate::frontend::app::App as Frontend;
 
 fn main() {
     let core = Core::new();
